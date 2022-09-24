4ee53ca9e1f16762fc4ae78f211e0f49513f9e01
GregoryGarber <ggarber@umass.edu>
2022-09-24 14:27:00 -0400

    env

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
+import dotenv  from "dotenv"
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cors())
+const port = process.env.PORT || 5000
 
 
 app.get('/test', (req, res) => {
@@ -14,6 +16,6 @@ app.get('/test', (req, res) => {
   });
 
 
-app.listen(5000, () => {
-    console.log('Listening on 5000');
-  });
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
